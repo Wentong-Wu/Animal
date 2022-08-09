10f71a05bd2a0b7dfebb9ba1e9aa5d13c1569bc1
Tom Wu <80329793+Wentong-Wu@users.noreply.github.com>
2022-08-09 16:57:19 +0100

    fixed test

--- src/test/java/org/test/CatTest.java
+++ src/test/java/org/test/CatTest.java
@@ -5,34 +5,28 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class CatTest {
+public class CatTest {
     Cat testCat = new Cat("Timmy", true, true,10);
     String currentExpectedName = testCat.getName();
     @Test
-    void test_get_cat_name(){
+    public void test_get_cat_name(){
         assertEquals(currentExpectedName,testCat.getName(),"getName method failed");
     }
+
     @Test
-    void test_set_cat_name(){
+    public void test_set_cat_name(){
         testCat.setName("Tommy");
         currentExpectedName = testCat.getName();
         assertEquals(currentExpectedName,testCat.getName(),"setName method failed");
     }
     @Test
-    void test_is_hair(){
-        assertEquals(true,testCat.isHair());
-    }
-    @Test
-    void test_set_hair(){
+    public void test_set_hair(){
         testCat.setHair(false);
         assertEquals(false,testCat.isHair());
     }
+
     @Test
-    void test_get_cat_power(){
-        assertEquals(10,testCat.getDamagePower());
-    }
-    @Test
-    void test_set_cat_power(){
+    public void test_set_cat_power(){
         testCat.setDamagePower(20);
         assertEquals(20,testCat.getDamagePower());
     }
